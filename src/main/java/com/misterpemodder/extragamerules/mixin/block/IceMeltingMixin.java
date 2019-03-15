47bd1ddeff52ec508e2ac557d80d6c6e380f8264
Yanis Guaye <yguaye44@gmail.com>
2019-03-15 04:09:25 +0100

    Split SnowIceMeltingMixin

--- src/main/java/com/misterpemodder/extragamerules/mixin/block/IceMeltingMixin.java
+++ src/main/java/com/misterpemodder/extragamerules/mixin/block/IceMeltingMixin.java
@@ -1,18 +1,17 @@
 package com.misterpemodder.extragamerules.mixin.block;
 
 import java.util.Random;
+import com.misterpemodder.extragamerules.hook.ServerWorldHook;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
-import com.misterpemodder.extragamerules.hook.ServerWorldHook;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.IceBlock;
-import net.minecraft.block.SnowBlock;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
-@Mixin(value = {SnowBlock.class, IceBlock.class})
-public class SnowIceMeltingMixin {
+@Mixin(IceBlock.class)
+public class IceMeltingMixin {
   @ModifyConstant(constant = @Constant(intValue = 11, ordinal = 0),
       method = "onScheduledTick(Lnet/minecraft/block/BlockState;Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Ljava/util/Random;)V")
   private int modifyMeltingLightLevel(int level, BlockState state, World world, BlockPos pos,
---
+++ src/main/java/com/misterpemodder/extragamerules/mixin/block/SnowMeltingMixin.java
@@ -0,0 +1,21 @@
+package com.misterpemodder.extragamerules.mixin.block;
+
+import java.util.Random;
+import com.misterpemodder.extragamerules.hook.ServerWorldHook;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.SnowBlock;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+@Mixin(SnowBlock.class)
+public class SnowMeltingMixin {
+  @ModifyConstant(constant = @Constant(intValue = 11, ordinal = 0),
+      method = "onScheduledTick(Lnet/minecraft/block/BlockState;Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Ljava/util/Random;)V")
+  private int modifyMeltingLightLevel(int level, BlockState state, World world, BlockPos pos,
+      Random random) {
+    return ((ServerWorldHook) world).getEGValues().snowMelts ? level : Integer.MAX_VALUE;
+  }
+}
